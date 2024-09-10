2c693110fd4cfdc5e26e37d35a0b16101ec052ee
Alexis Salgado <chimalexoficial@gmail.com>
2024-09-10 16:31:58 -0600

    adding Spring core

---
+++ 01-spring-boot-overview/06-properties-demo/src/main/java/com/luv2code/springboot/demo/mycoolapp/rest/FunRestController.java
@@ -0,0 +1,41 @@
+package com.luv2code.springboot.demo.mycoolapp.rest;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FunRestController {
+
+    // inject properties (check application.properties)
+    @Value("${coach.name}")
+    private String coachName;
+
+    @Value("${team.name}")
+    private String teamName;
+
+    // new endpoint
+    @GetMapping("/teaminfo")
+    public String getTeamInfo() {
+        return "Coach: " + coachName + " - " + "Team name is: " + teamName;
+    }
+
+    // expose "/" that return a "Hello World!"
+    @GetMapping("/")
+    public String sayHello() {
+        return "Hello World!";
+    }
+
+    // expose a new endpoint for "workout"
+    @GetMapping("/workout")
+    public String getDailyWorkout() {
+        return "Run 5k!";
+    }
+
+    // expose a new endpoint for "fortune"
+    @GetMapping("/fortune")
+    public String getDailyFortune() {
+        return "Today is your lucky day";
+    }
+
+}
